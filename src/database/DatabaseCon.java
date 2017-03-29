917b7429fbb7e7bc6e51466d50b12481f6bc7130
lovevol <935686942@qq.com>
2017-03-29 10:15:18 +0800

    database

---
+++ src/database/DatabaseCon.java
@@ -0,0 +1,65 @@
+package database;
+
+import java.sql.*;
+
+/**
+ * Created by lh
+ * on 2017/3/29.
+ */
+
+public class DatabaseCon {
+
+    private Connection connection = null;
+    private Statement statement= null;
+    private ResultSet resultSet = null;
+    public DatabaseCon(){
+        String usrName = "sa";
+        String password = "935686942";
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String url = "jdbc:sqlserver://localHost:1433;DatabaseName=SCHOOL";
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url,usrName,password);
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public ResultSet executeQuery(String sql){
+        try {
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+    public int executeUpdate(String sql){
+        int rs = 0;
+        try {
+            statement = connection.createStatement();
+            rs = statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public void closeStatement(){
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
